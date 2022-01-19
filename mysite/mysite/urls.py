9fbca2134bac0ae8d55c47dd276791b9997d4c59
camilo1rp <camilo1rp@gmail.com>
2022-01-19 11:19:22 -0500

    changed: main index to open poll list. changed: index view to query all the questions

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from polls import views
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls)
 ]
